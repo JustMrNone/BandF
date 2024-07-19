a351c0edec855a033fea3723eea011226c9e848a
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-07-19 10:57:50 +0330

    "Updated templates, added navigation bar and styles, created menu view and URL"

--- resturant/views.py
+++ resturant/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'resturant/index.html')
+
+def menu(request):
+    return render(request, 'resturant/menu.html')
